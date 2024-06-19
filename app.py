2928ab7b6139abd68a25b73bc438fa2899bfb203
Szymon Klim <277461@student.pwr.edu.pl>
2024-06-19 14:02:17 +0200

    dodane zakładki do poszczególnych tematów

--- app.py
+++ app.py
@@ -18,6 +18,38 @@ def contact_us():
 def spis():
     return render_template('spis.html')
 
+@app.route('/temat2')
+def temat2():
+    return render_template('topics/temat2.html')
+
+@app.route('/temat1')
+def temat1():
+    return render_template('topics/temat1.html')
+
+@app.route('/temat3')
+def temat3():
+    return render_template('topics/temat3.html')
+
+@app.route('/temat4')
+def temat4():
+    return render_template('topics/temat4.html')
+
+@app.route('/temat5')
+def temat5():
+    return render_template('topics/temat5.html')
+
+@app.route('/temat6')
+def temat6():
+    return render_template('topics/temat6.html')    
+
+@app.route('/temat7')  
+def temat7():
+    return render_template('topics/temat7.html')    
+
+@app.route('/temat8')   
+def temat8():
+    return render_template('topics/temat8.html')    
+
 if __name__ == '__main__':
     app.run(debug=True)
 
